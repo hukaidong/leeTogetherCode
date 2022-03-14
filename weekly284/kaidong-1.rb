052f9a4d19a80391d3387d9d26c6f89ff8579fd7
Mr_Melovdy <hukaidonghkd@gmail.com>
2022-03-13 23:31:33 -0400

    Update kaidong-1.rb

--- weekly284/kaidong-1.rb
+++ weekly284/kaidong-1.rb
@@ -3,18 +3,8 @@
 # @param {Integer} k
 # @return {Integer[]}
 def find_k_distant_indices(nums, key, k)
-    idxs = nums.map.with_index do |x, i|
-        if x == key
-            i
-        else
-            nil
-        end
-    end.compact
-    idxs.map do |x|
-        startIdx = x - k
-        startIdx = startIdx > 0 ? startIdx : 0
-        eIdx = x + k
-        eIdx = eIdx < nums.length ? eIdx : nums.length - 1
-        (startIdx..eIdx).to_a
-    end.flatten.uniq
+    idxs = nums.each_index.select {|x| nums[x] == key}
+    idxs.flat_map {|x| (x-k..x+k).to_a}
+        .uniq
+        .select {|x| x.between?(0, nums.length-1)}
 end
